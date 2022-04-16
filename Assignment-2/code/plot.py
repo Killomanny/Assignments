2b49b66de2cf26dda52a48dc021172ea662b2986
AsliNitejReddy <102515413+Killomanny@users.noreply.github.com>
2022-04-16 11:36:25 +0530

    Update plot.py

--- Assignment-2/code/plot.py
+++ Assignment-2/code/plot.py
@@ -1,1 +1,13 @@
+import matplotlib.pyplot as plt
+import numpy as np
 
+x = np.linspace(-5, 15, 20)
+y = 5 * (x + 1) / 4
+
+plt.plot(x, y, '-r', label='4y=5x+5')
+plt.xlabel('x', color='k')
+plt.ylabel('y', color='k')
+plt.legend(loc='upper left')
+plt.grid()
+
+plt.show()
