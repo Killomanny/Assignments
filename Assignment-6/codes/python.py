834db678a24a732428c1e79a184f4be76bd43091
AsliNitejReddy <102515413+Killomanny@users.noreply.github.com>
2022-05-11 16:32:18 +0530

    Update python.py

--- Assignment-6/codes/python.py
+++ Assignment-6/codes/python.py
@@ -1,1 +1,24 @@
-hello
+# B.ASLI
+# CS21BTECH11011
+# PROBABILITY
+
+
+# probability that A occur ( 1st child is girl )
+p_a = float(0.50)
+
+# probability that B occur ( 2nd child is girl )
+p_b = float(0.50)
+
+# probability that both occur ( 1st child and 2nd child both are girls )
+p_ab = float(p_a*p_b)
+
+
+# probability that atleast one of A or B occur
+p_aoc = float(p_a + p_b - p_ab)
+
+p_boga = float(p_ab) / p_aoc
+p_boga = "{:.2f}".format(p_boga)
+
+print("probability that both occur given that B occurs is ", p_a)
+
+print("probability that both occur given that at least one occurs is ", p_boga)
