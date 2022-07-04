307dfe1a5b3550732dab516aeaad8bdb3c6fe1e6
AsliNitejReddy <102515413+Killomanny@users.noreply.github.com>
2022-07-04 10:20:51 +0530

    Create 2.2.py

---
+++ Assignmets2.0/codes/2.2.py
@@ -0,0 +1,32 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import mpmath as mp
+simlen = 1000000
+randvar = np.loadtxt('./gau.dat',dtype='double')
+
+
+x = np.linspace(-5,5,40)
+CF=[]
+for i in range(0,40):
+	CF_ind = np.nonzero(randvar < x[i]) 
+	CF_n = np.size(CF_ind)
+	CF.append(CF_n/simlen) 
+	
+	
+def Q(x):
+    return (1-mp.erf(x/mp.sqrt(2)))/2
+    
+def f(x):
+    return 1-Q(x)
+    
+cdf = np.vectorize(f)
+
+plt.plot(x.T,cdf(x))
+plt.plot(x[0:40].T,CF,"o")
+plt.grid()
+plt.xlabel('$x$')
+plt.ylabel('$F_X(x)$')
+plt.legend(["Theory","Numerical"])
+plt.savefig("../figs/gauss_cdf.pdf")
+plt.savefig("../figs/gauss_cdf.eps")
+plt.show()
