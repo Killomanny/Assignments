358f8401a01f748a47fe0628a74559b7f5936c26
AsliNitejReddy <102515413+Killomanny@users.noreply.github.com>
2022-07-04 10:18:37 +0530

    Create 1.4.c

---
+++ Assignmets2.0/codes/1.4.c
@@ -0,0 +1,17 @@
+17 lines (14 sloc)  295 Bytes
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "source.h"
+double variance(char *str)
+{
+return (meansquare(str)-mean(str)*mean(str));
+}
+
+int  main(void) //main function begins
+{
+printf("mean =%lf\n",mean("uni.dat"));
+printf("variance=%lf\n",variance("uni.dat"));
+return 0;
+}
