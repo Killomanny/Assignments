b9b9edf84286c3906ad4e503cb0b3ef4d33fe3a0
AsliNitejReddy <102515413+Killomanny@users.noreply.github.com>
2022-07-04 10:27:19 +0530

    Create 5.2.c

---
+++ Assignmets2.0/codes/5.2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "source.h"
+int  main(void) //main function begins
+{
+ 
+//Uniform random numbers
+combo("combo.dat", 1000000);
+return 0;
+}
