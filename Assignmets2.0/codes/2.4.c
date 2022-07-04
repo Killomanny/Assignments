e50b60c68151c90f7b3b8ca494b997544c238d21
AsliNitejReddy <102515413+Killomanny@users.noreply.github.com>
2022-07-04 10:22:09 +0530

    Create 2.4.c

---
+++ Assignmets2.0/codes/2.4.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "source.h"
+double variance(char *str)
+{
+return (meansquare(str)-mean(str)*mean(str));
+}
+
+int  main(void) //main function begins
+{
+printf("mean =%lf\n",mean("gau.dat"));
+printf("variance=%lf\n",variance("gau.dat"));
+return 0;
+}
