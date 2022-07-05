79d772d358cec4e9d174ac9d34160b437c91c169
AsliNitejReddy <102515413+Killomanny@users.noreply.github.com>
2022-07-05 11:47:35 +0530

    Update 1.4.c

--- Assignmets2.0/codes/1.4.c
+++ Assignmets2.0/codes/1.4.c
@@ -2,14 +2,18 @@
 #include <stdlib.h>
 #include <math.h>
 #include "source.h"
-double variance(char *str)
-{
+
+double variance(char *str){
+  
 return (meansquare(str)-mean(str)*mean(str));
+  
 }
 
-int  main(void) //main function begins
-{
+int main(void){
+  
 printf("mean =%lf\n",mean("uni.dat"));
 printf("variance=%lf\n",variance("uni.dat"));
+  
 return 0;
+  
 }
