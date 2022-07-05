8a2c52047ba8c7822a3e24d7c2b63f4ed33f1bb4
AsliNitejReddy <102515413+Killomanny@users.noreply.github.com>
2022-07-05 11:53:40 +0530

    Update 2.1.c

--- Assignmets2.0/codes/2.1.c
+++ Assignmets2.0/codes/2.1.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+
 #include "source.h"
-int  main(void) //main function begins
-{
+
+int  main(void){
  
 gaussian("gau.dat", 1000000);
-
 return 0;
 }
