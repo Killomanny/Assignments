058de61a878a04590f50b4a08928f5faddcbcf16
AsliNitejReddy <102515413+Killomanny@users.noreply.github.com>
2022-07-05 11:52:28 +0530

    Update 1.5cal.py

--- Assignmets2.0/codes/1.5cal.py
+++ Assignmets2.0/codes/1.5cal.py
@@ -7,7 +7,11 @@ def f(x):
     return x
 def g(x):
     return x*x;
+  
+  
 x = sy.Symbol("x")
+
 print("E(U)=",sy.integrate(f(x), (x, 0, 1)))
 print("E(U^2)=",sy.integrate(g(x), (x, 0, 1)))
+
 print("Variance=",(sy.integrate(g(x), (x, 0, 1))-sy.integrate(f(x), (x, 0, 1))*sy.integrate(f(x), (x, 0, 1))))
