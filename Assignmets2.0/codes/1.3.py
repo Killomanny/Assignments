5f2faaba0286f81680bfa036d88208282393f94a
AsliNitejReddy <102515413+Killomanny@users.noreply.github.com>
2022-07-05 11:37:48 +0530

    Update 1.3.py

--- Assignmets2.0/codes/1.3.py
+++ Assignmets2.0/codes/1.3.py
@@ -1,13 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import pandas as pd
+
 
 pts = 50
-n = 10**6
-U = pd.read_csv("../1.1/uni.dat")
-
-x = np.linspace(-2,2,pts)
+x = np.linspace(-4,4,pts)
 F = []
+
 
 def uniform_cdf(x):
         if(x<0):
@@ -17,15 +15,10 @@ def uniform_cdf(x):
         else:
                 return x
 
-F_theory=[]
 
 vec_uniform = np.vectorize(uniform_cdf)
 
-for i in range(0,pts):
-        F_n = np.count_nonzero(U < x[i]) #checking probability condition
-        F.append(F_n/n) #storing the probability values in a list
 
-plt.scatter(x.T, F, color="blue" )#plotting the CDF
 plt.plot(x.T, vec_uniform(x), color="orange" )#plotting the CDF
 plt.grid()
 plt.legend()
