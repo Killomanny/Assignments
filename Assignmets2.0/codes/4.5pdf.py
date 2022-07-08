53227013663ea1da28affe43f05a6454ed8bc383
AsliNitejReddy <102515413+Killomanny@users.noreply.github.com>
2022-07-08 12:12:35 +0530

    Update 4.5pdf.py

--- Assignmets2.0/codes/4.5pdf.py
+++ Assignmets2.0/codes/4.5pdf.py
@@ -4,36 +4,50 @@ import pandas as pd
 import math
 
 pts = 60
-n = 10 ** 6
 
+n = 10**6
+X = np.fromfile("tri.dat", dtype=float, count= n, sep='', offset=0)
 
+x = np.linspace(-3,3,pts)
 
-X = np.fromfile("tri.dat", dtype=float, count=n, sep='', offset=0)   #this is big X
-x = np.linspace(-3, 3, pts)
-
-
-
-# Empirical PDF
-P = []
-for i in range(0, pts - 1):
-    P.append((F[i + 1] - F[i]) / (x[i + 1] - x[i]))
-
-
-
-# Theoretical PDF
+#Empirical CDF
+F = [] 
+for i in range(0,pts):
+	Fcount = np.count_nonzero(X < x[i]) 
+	F.append(Fcount/n) 
+	
+#Empirical PDF
+p = []
+for i in range(0,pts-1):
+	p.append( (F[i+1] - F[i])/ (x[i+1]-x[i]) )
+	
+#Theoretical PDF
 def gen_p_theory(xi):
-    if (xi <= 0):
-        return 0.0
-    elif (xi <= 1):
-        return xi
-    elif (xi <= 2):
-        return 2 - xi
-    else:
-        return 0.0
-
-
+	if (xi <= 0):
+		return 0.0 
+	elif (xi <= 1):
+		return xi 
+	elif (xi <= 2):
+		return 2-xi
+	else:
+		return 0.0
+	
 vecgen_p_theory = np.vectorize(gen_p_theory)
 p_theory = vecgen_p_theory(x)
+
+#Theoretical CDF
+def gen_F_theory(xi):
+	if (xi <= 0):
+		return 0.0 
+	elif (xi <= 1):
+		return xi*xi/2 
+	elif (xi <= 2):
+		return -xi*xi/2 + 2 * xi -1
+	else:
+		return 1.0
+
+vecgen_F_theory = np.vectorize(gen_F_theory)
+F_theory = vecgen_F_theory(x)
 
 
 
