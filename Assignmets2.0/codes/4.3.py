6bab3e445cd0a3888f47d04b5dc98192b8191a98
AsliNitejReddy <102515413+Killomanny@users.noreply.github.com>
2022-07-08 10:33:58 +0530

    Update 4.3.py

--- Assignmets2.0/codes/4.3.py
+++ Assignmets2.0/codes/4.3.py
@@ -1,43 +1,23 @@
-#Importing numpy, scipy, mpmath and pyplot
-import numpy as np
-import mpmath as mp
-import scipy 
 import matplotlib.pyplot as plt
+import numpy as np
 
+T=np.loadtxt("../4.1/tri.dat")
+pts=100
+N=10**6
 
+x=np.linspace(-6,6,pts)
 
-maxrange=50
-maxlim=6
-x = np.linspace(-maxlim+2,6,maxrange)#points on the x axis
-simlen = int(1e6) #number of samples
-err = [] #declaring probability list
-pdf = [] #declaring pdf list
-h = 2*maxlim/(maxrange-1);
-#randvar = np.random.normal(0,1,simlen)
-#randvar = np.loadtxt('uni.dat',dtype='double')
-randvar = np.loadtxt('tri.dat',dtype='double')
+F=[]
+for i in range(0,pts):
+        F.append(np.size(np.nonzero(T < x[i]))/N)
 
-for i in range(0,maxrange):
-	err_ind = np.nonzero(randvar < x[i]) #checking probability condition
-	err_n = np.size(err_ind) #computing the probability
-	err.append(err_n/simlen) #storing the probability values in a list
+p=[]
+for i in range(0,pts-1):
+	p.append((F[i+1]-F[i])/(x[i+1]-x[i]))
 
-	
-for i in range(0,maxrange-1):
-	test = (err[i+1]-err[i])/(x[i+1]-x[i])
-	pdf.append(test) #storing the pdf values in a list
-
-def tri_pdf(x):
-	return 1/mp.sqrt(2*np.pi)*np.exp(-x**2/2.0)
-	
-tri_pdf = scipy.vectorize(tri_pdf)
-
-plt.plot(x[0:(maxrange-1)].T,pdf,'o')
-#plt.plot(x,tri_pdf(x))#plotting the CDF
-plt.grid() #creating the grid
-plt.xlabel('$x_i$')
-plt.ylabel('$p_X(x_i)$')
-plt.legend(["Numerical","Theory"])
-
-
-plt.show() #opening the plot window
+plt.plot(x[0:pts-1],p,label="PDF")
+plt.grid()
+plt.xlabel("x")
+plt.ylabel("$p_T(x)$")
+plt.legend()
+plt.show()
